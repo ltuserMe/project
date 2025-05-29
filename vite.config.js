e3723c9eff7bd1c32992ec03e357a0a12be77a05
Ituserme <17821008188@163.com>
2025-05-29 16:37:46 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,11 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: "3001",
-    proxy: {
-      "^/api": {
-        target: "http://localhost:3000/",
-        // rewrite: (path) => path.replace(new RegExp("^" + "/dev-api"), ""),
-      },
+    // proxy: {
+    //   "^/api": {
+    //     target: "http://47.120.6.86:5000/",
+    //     // rewrite: (path) => path.replace(new RegExp("^" + "/dev-api"), ""),
+    //   },
     },
   },
   resolve: {
