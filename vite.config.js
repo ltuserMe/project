54e95966bb846fbd0e497876579525c39efb9790
zhangpeng <zhangpeng@pintechs.com>
2025-05-29 14:40:04 +0800

    fix: 修改后端地址

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   server: {
     host: "0.0.0.0",
-    port: "3000"
+    port: "3001",
+    proxy: {
+      "^/api": {
+        target: "http://localhost:3000/",
+        // rewrite: (path) => path.replace(new RegExp("^" + "/dev-api"), ""),
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
